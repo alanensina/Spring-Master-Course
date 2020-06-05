058db2f37bbbb0e3d45fe106216cee8400520215
Alan Ensina <alan.ensina@grad.ufsc.br>
2020-06-05 18:21:38 -0300

    Request param and mapping

---
+++ spring-web-mvc/WebContent/WEB-INF/view/silly.jsp
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+<html>
+	<body>
+		<h2>Just a silly page</h2>
+	</body>
+</html>
--- spring-web-mvc/src/controllers/HelloWorldController.java
+++ spring-web-mvc/src/controllers/HelloWorldController.java
@@ -5,8 +5,10 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/hello")
 public class HelloWorldController {
 
     // Show the initial HTML form
@@ -30,6 +32,19 @@ public class HelloWorldController {
 
         // create message
         String result = "Yo! " + theName + ", this is Spring Framework dude!";
+
+        // add message to the model
+        model.addAttribute("message", result);
+
+        return "helloworld";
+    }
+
+    // New controller to read form data and add data to the model
+    @RequestMapping("/processFormV3")
+    public String processFromVersionThree(@RequestParam("studentName") String name, Model model) {
+
+        // create message
+        String result = "Hello my friend " + name.toUpperCase() + ", this is the version 3!";
 
         // add message to the model
         model.addAttribute("message", result);
